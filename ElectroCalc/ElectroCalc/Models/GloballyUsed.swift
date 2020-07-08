b189d4b45f08e32825cef16a5c8f41a812bddcae
JerExplosionNextGeneration <67714039+JerExplosionNextGeneration@users.noreply.github.com>
2020-07-07 23:06:50 -0400

    ElectroCalc .init( )

---
+++ ElectroCalc/ElectroCalc/Models/GloballyUsed.swift
@@ -0,0 +1,15 @@
+//
+//  GloballyUsed.swift
+//  ElectroCalc
+//
+//  Created by Jerry Ren on 7/7/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import Foundation
+
+struct GloballyUsed {
+    static let ElectroCalcSBoardID
+        = "ElectroCalcViewController"
+}
+           
---
+++ ElectroCalc/ElectroCalc/ViewControllers/ElectroCalcViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ElectroCalcViewController.swift
+//  ElectroCalc
+//
+//  Created by Jerry Ren on 7/7/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+class ElectroCalcViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .some(UIColor.black)
+    }
+    
+} 
