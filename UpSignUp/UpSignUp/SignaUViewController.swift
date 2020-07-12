682d5fde6d9a7214caa43ca7d8641b7ff17b4570
JerExplosionNextGeneration <67714039+JerExplosionNextGeneration@users.noreply.github.com>
2020-07-11 23:12:02 -0400

    $~UpSignUp~$
    $~UpSignUp~$

---
+++ UpSignUp/UpSignUp/SignaUViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SignaUViewController.swift
+//  UpSignUp
+//
+//  Created by Jerry Ren on 7/10/20.
+//  Copyright © 2020 Jerry Ren. All rights reserved.
+//
+
+import UIKit
+
+private class SignaUViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()}
+}
+
